d6a9669754c671ba00303c5b82fc0e5c57beae8b
Varun <varunn.pandey@gmail.com>
2021-04-28 01:36:59 +0530

    Setting up prettier and eslint

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-const { response } = require('express');
-const express=  require('express')
-const path=require('path')
+const { response } = require("express");
+const express = require("express");
+const path = require("path");
 
 //Call the  function
-const app=  express();
+const app = express();
 
-// To serve static files such as images, 
+// To serve static files such as images,
 //CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 
 //  This  is a middleware. Everytime app.use--  it is a middle ware
-app.use(express.static('static'))
-
+app.use(express.static("static"));
 
 //Define the port to  listen to
-const port=3000
+const port = 3000;
 
-var arr=[3000,2000]
+var arr = [3000, 2000];
 
 // listen to the port
-var server= app.listen(port,()=>{
-    console.log(`server is listening ${port}`)
-    console.log('server  is lisetning  to '+port)
-})
+var server = app.listen(port, () => {
+  console.log(`server is listening ${port}`);
+  console.log("server  is lisetning  to " + port);
+});
 
 //Define the  route which takes a call back
-app.get('/home',((req,res)=>{
-    res.sendFile(path.join(__dirname,'./static/index.html'));
-//res.send("This is my  home  :)"+arr)
-})
-)
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "./static/index.html"));
+  //res.send("This is my  home  :)"+arr)
+});
