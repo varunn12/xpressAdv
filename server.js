3546a557091f01cb0191e6e3ede296f9710e2470
Varun <varunn.pandey@gmail.com>
2021-04-28 01:43:54 +0530

    Eslint error fix

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const { response } = require("express");
 const express = require("express");
 const path = require("path");
 
-//Call the  function
+// Call the  function
 const app = express();
 
 // To serve static files such as images,
-//CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+// CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 
 //  This  is a middleware. Everytime app.use--  it is a middle ware
 app.use(express.static("static"));
 
-//Define the port to  listen to
+// Define the port to  listen to
 const port = 3000;
 
-var arr = [3000, 2000];
+// let arr = [3000, 2000];
 
 // listen to the port
-var server = app.listen(port, () => {
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`server is listening ${port}`);
+  // eslint-disable-next-line no-console
   console.log("server  is lisetning  to " + port);
 });
 
-//Define the  route which takes a call back
+// Define the  route which takes a call back
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "./static/index.html"));
-  //res.send("This is my  home  :)"+arr)
+  // res.send("This is my  home  :)"+arr)
 });
